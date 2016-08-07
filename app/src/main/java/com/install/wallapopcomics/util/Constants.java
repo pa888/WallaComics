c648648c4355045eb6a88cd74c9899a94bbc0dfc
pa888 <peter.zverkov888@gmail.com>
2016-08-08 00:04:51 +0200

    changed hero

--- app/src/main/java/com/install/wallapopcomics/util/Constants.java
+++ app/src/main/java/com/install/wallapopcomics/util/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final String HASH = "hash";
     public static final String TIMESTAMP = "ts";
     public static final String EXTENSION_DOT = ".";
-    public static final String CHARACTER_ID = "1009220";//1009168 Banshee//"1009220";//C_AMERICA
+    public static final String CHARACTER_ID = "1009168";//1009168 Banshee//"1009220";//C_AMERICA
 
     public static final String PUBLIC_KEY = "c56b52307eac73203472a46e18c2fa53";
     public static final String PRIVATE_KEY = "a070311f7a0d198905d8d07b8a7490ee6781536a";
